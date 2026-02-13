383c24f99354d22742f2482cbe6b55b87fd48fe4
Shiku1228 <latanggar2@gmail.com>
2026-02-13 12:07:19 +0800

    Firebase integrated and synced.

---
+++ app/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    id("com.android.application")
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.crossplatmultifacauth"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.crossplatmultifacauth"
+        minSdk = 34
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+
+    //firebase dependencies=======================================================
+    //Firebase BOM (manage the version of the firebase)
+    implementation(platform("com.google.firebase:firebase-bom:33.9.0"))
+
+    //Firebase Authentication (for the log in implementation)
+    implementation("com.google.firebase:firebase-auth")
+
+    //Firebase Firestore
+    implementation("com.google.firebase:firebase-firestore")
+
+    implementation("com.google.firebase:firebase-analytics")
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
